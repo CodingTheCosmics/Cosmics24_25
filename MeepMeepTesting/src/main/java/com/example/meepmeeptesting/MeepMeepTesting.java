dcd0acb9ffb6dec516da11e724811603b192db9f
CodingTheCosmics <codingthecosmics@gmail.com>
2024-10-17 22:31:28 -0500

    experimenting!!

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseMap;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -15,13 +16,19 @@ public class MeepMeepTesting {
                 .setConstraints(45, 30, 4.639, Math.toRadians(30), 11.35)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-16, 65, Math.toRadians(90)))
-                .splineTo(new Vector2d(-46, 65), 0)
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(16, 65, Math.toRadians(0)))
+                .strafeTo(new Vector2d(46, 65))
+                //insert marker to drop preloaded sample into bucket
+                .splineTo(new Vector2d(55, 40), Math.toRadians(-90))
+                //insert marker to pick up new sample
+                .lineToY(50)
+                .turnTo(Math.toRadians(-270))
+                //insert marker to drop sample into bucket
 
 
                 .build());
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
